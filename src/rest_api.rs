50b3889437fa810cedb610b381f2e6cd98867dcd
Christopher Schmidt <christopher.schmidt@posteo.de>
2018-09-30 00:10:01 +0200

    Test travis integration

--- src/rest_api.rs
+++ src/rest_api.rs
@@ -4,3 +4,12 @@ pub const VERSION: Option<&'static str> = option_env!("CARGO_PKG_VERSION");
 pub fn version() -> &'static str {
     VERSION.unwrap_or("unknown")
 }
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
